7aa4aba88ddef10eccd2cdd6121817fed2fd850e
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2019-10-30 21:40:07 -0300

    commit

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User, UserProfile
+from .models import User, UserProfile, Car
 
+admin.site.register(Car)
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
